117ea5bcbacb28140457661f9e4a13d30eacdee3
Jon Nilsen <40994635+jonaagenilsen@users.noreply.github.com>
2018-07-13 09:21:38 +0200

    Update test-codius-upload.sh

--- test-codius-upload.sh
+++ test-codius-upload.sh
@@ -1,7 +1,36 @@
+#
+# Required packages:
+#
+# yum install bind-utils -y -q
+#
+
+usage () {
+  echo -e "Usage: test-codius-upload.sh \e[93m<hostname>\e[0m"
+  echo -e "- Example #1: \e[0;32mtest-codius-upload.sh\e[0m \e[93mlocal\e[0m          (test your local server)"
+  echo -e "- Example #2: \e[0;32mtest-codius-upload.sh\e[0m \e[93mhodling-xrp.no\e[0m (test remote hostname)"
+  exit
+}
+
+HOST=$1
+
+if [ "$HOST" != '' ]; then
+  if [ "$HOST" != 'local' ]; then
+    host $HOST
+    if [ "$?" -eq "0" ]; then
+      echo "$HOST resolves with DNS. Continuing script."
+      echo
+    else
+      usage
+    fi
+  fi
+else
+  usage
+fi
+
 cd
 npm install -g codius
 
-POD="/root/upload-test"
+POD="/root/scripts/upload-test"
 mkdir -p $POD
 
 cat << EOF > $POD/codius.json
@@ -28,6 +57,14 @@ cat << EOF > $POD/codiusvars.json
 EOF
 
 cd $POD
-codius upload --host https://`hostname` --duration 30 -o -y
+if [ "$HOST" == 'local' ]; then
+  HOSTNAME=`hostname`
+else
+  HOSTNAME=$HOST
+fi
+
+echo -e "Uploading and testing server: \e[0;32m$HOSTNAME\e[0m"
+echo
+codius upload --host https://$HOSTNAME --duration 30 -o -y
 
 rm -rf $POD
